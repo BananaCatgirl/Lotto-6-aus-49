2b417eecd819f849202d24fd22bb6cb677069051
Bananatoast <50334579+BananaCatgirl@users.noreply.github.com>
2025-05-05 11:56:51 +0200

    update für Lottospiel.py und der Lottokarte

--- LottoKarte/LottoKarte.py
+++ LottoKarte/LottoKarte.py
@@ -1,4 +1,6 @@
 class LottoKarte:
 	def __init__(self):
-		self.
+		pass
+
+	def Setup():
 		pass
--- Lottospiel.py
+++ Lottospiel.py
@@ -9,12 +9,39 @@ from Ziehung.Ziehung import Ziehung
 from Kasse.Kasse import Kasse
 
 
+class Lottospiel:
+	def __init__(self):
+		self.Lottokarten = []
+		self.Kasse = Kasse()
+		self.Ziehung = Ziehung()
+
+	def StartLottoKarten(self):
+		try:
+			pass
+		except:
+			print("Fehler beim ausführen der KartenLogic")
 
 
+	def StartKasse(self):
+		try:
+			pass
+		except:
+			print("Fehler beim ausführen der Kasse")
+
+
+	def StartZiehung(self):
+		try:
+			pass
+		except:
+			print("Fehler beim ausführen der Ziehung")
 
 
 def main():
 	while True:
+		HauptSpiel = Lottospiel()
+		HauptSpiel.StartLottoKarten()
+		HauptSpiel.StartKasse()
+		HauptSpiel.StartZiehung()
 		pass
 
 if __name__ == "main":
