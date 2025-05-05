28adb04e575df9e4d6ea074aa15235ed08cd816a
Bananatoast <50334579+BananaCatgirl@users.noreply.github.com>
2025-05-05 09:18:54 +0200

    initial commit

---
+++ Lottospiel.py
@@ -0,0 +1,16 @@
+
+
+
+class Programm:
+	
+	def __init__(self):
+		self.runProgramm = True
+
+	def main():
+		print("Lotto programm gestartet")
+
+
+
+if __name__ == "main":
+	print("Wilkommen zu Lotto 6 aus 49")
+	programm = Programm()
