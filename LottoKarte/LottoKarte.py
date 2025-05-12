4926091c04bf0dd776eaf7f6a42b8ff9b5e81e29
Bananatoast <50334579+BananaCatgirl@users.noreply.github.com>
2025-05-12 10:47:10 +0200

    added gitignore and simple methods for value getting

--- LottoKarte/LottoKarte.py
+++ LottoKarte/LottoKarte.py
@@ -4,5 +4,19 @@ class LottoKarte:
 		self.mittwochsZiehung = False
 		self.SamstagsZiehung = False
 
-	def Setup(self):
+	def Setup(self):# wieviele spiele aus 18max? welche losungs tage?
 		pass
+
+	def GetSpiele(self) -> int:
+		return len(self.spiele)
+	
+	def GetZiehungsTage(self):
+		return (self.mittwochsZiehung,self.SamstagsZiehung)
+	
+	def GetZiehungsTagMittwoch(self)->bool:
+		return self.mittwochsZiehung
+	
+	def GetZiehungsTagSamstag(self)->bool:
+		return self.SamstagsZiehung
+	
+	
